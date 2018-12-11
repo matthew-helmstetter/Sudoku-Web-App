4b3d1ebd41fe201448ddc272d06404d0e6a0e7b3
matthew-helmstetter <mhelmstetter@berkeley.edu>
2018-12-10 22:23:19 -0800

    Updates

---
+++ precache-manifest.01a165abc43d79c618b56fac47f9ccef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Sudoku-Web-App/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d3725be2ac1657ed1966",
+    "url": "/Sudoku-Web-App/static/js/runtime~main.d3725be2.js"
+  },
+  {
+    "revision": "c8549ed376025e84bd48",
+    "url": "/Sudoku-Web-App/static/js/main.c8549ed3.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/Sudoku-Web-App/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "c8549ed376025e84bd48",
+    "url": "/Sudoku-Web-App/static/css/main.e637c562.chunk.css"
+  },
+  {
+    "revision": "800d6a7db72295e8c8880597b5053a94",
+    "url": "/Sudoku-Web-App/index.html"
+  }
+];
